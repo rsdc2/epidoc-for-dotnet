56b1fe0a086f74be144ff1db6e3b4351cd2d5663
Robert Crellin <54702776+rsdc2@users.noreply.github.com>
2025-01-27 21:04:16 +0000

    Add Health Controller

---
+++ EpiDocAPI/Controllers/HealthController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EpiDocAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HealthController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Health()
+        {
+            return Ok("Server is alive");
+        }
+    }
+}
